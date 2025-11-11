0cb90d951f638b13a752b1e5611c3f6db493e6d8
vinita-devops <137490163+vinita-devops@users.noreply.github.com>
2025-11-11 13:06:39 +0530

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/user', (req,res)=>{
+    res.send({'msg':'Microservice 4: User Said Namaste'})
+})
+
+app.get('/', (req,res)=>{
+    res.send({'msg':'Hello'})
+})
+
+app.get('/health', (req,res)=>{
+    res.send({'msg':'OK!'})
+})
+
+app.listen(3003, ()=>{
+    console.log('Microservice 4 is listening at port 3003')
+})
